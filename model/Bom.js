7cb30df603bb1550ecc5dd7e5d1fccca48276ee2
D074360 <manjunath.mandya.surendrakumar@sap.com>
2022-01-20 12:15:38 +0100

    Minimizing changes

--- model/Bom.js
+++ model/Bom.js
@@ -16,6 +16,7 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright (c) OWASP Foundation. All Rights Reserved.
  */
+
 const builder = require('xmlbuilder')
 const uuid = require('uuid')
 const Component = require('./Component')
