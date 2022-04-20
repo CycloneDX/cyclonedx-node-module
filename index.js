dd77dccbdba68783d66dd7e03e76fabd02c86911
Evan Merz <evanxmerz@yahoo.com>
2022-04-20 11:26:33 -0700

    Refactor to create BOMs from text.

--- index.js
+++ index.js
@@ -22,29 +22,61 @@ const filePath = require('path')
 const readInstalled = require('read-installed')
 const { yarnToNpm } = require('synp')
 
-const Bom = require('./model/Bom')
+const Bom = require('./model/Bom') // do I just want to use this model?
 
-exports.createbom = (componentType, includeSerialNumber, includeLicenseText, path, options, callback) => readInstalled(path, options, (err, pkgInfo) => {
-  if (err) { callback(err, null); return }
+readPackageLock = (path) => {
+  let lockfileContents = "";
 
-  let lockfile
   if (fs.existsSync(filePath.join(path, 'package-lock.json'))) {
-    lockfile = JSON.parse(fs.readFileSync(filePath.join(path, 'package-lock.json')))
+    lockfileContents = JSON.parse(fs.readFileSync(filePath.join(path, 'package-lock.json')))
   } else if (fs.existsSync(filePath.join(path, 'yarn.lock'))) {
     // Convert the yarn lock file to a package-lock.json string, prior to parsing JSON.
-    lockfile = JSON.parse(yarnToNpm(path))
+    lockfileContents = JSON.parse(yarnToNpm(path))
   }
 
   // Add a console warning for users that have both npm and yarn lock files.
   if (fs.existsSync(filePath.join(path, 'package-lock.json')) && fs.existsSync(filePath.join(path, 'yarn.lock'))) {
     console.warn('Please review your project as multiple package management lock files exist, defaulting to package-lock.json')
-  }
+  } 
 
-  const bom = new Bom(pkgInfo, componentType, includeSerialNumber, includeLicenseText, lockfile)
+  return lockfileContents;
+}
+
+createbomFromFile = (componentType, includeSerialNumber, includeLicenseText, path, options, callback) => {
+  readInstalled(path, options, (err, pkgInfo) => {
+    // if there was an error
+    if (err) { callback(err, null); return }
+
+    const lockFileContents = readPackageLock(path);
+
+    const bom = new Bom(pkgInfo, componentType, includeSerialNumber, includeLicenseText, lockFileContents)
+
+    callback(null, bom)
+  });
+};
+
+createbomFromText = (componentType, includeSerialNumber, includeLicenseText, lockFileContents, options, callback) => {
+  readInstalled(path, options, (err, pkgInfo) => {
+    // if there was an error then call the callback with the error
+    if (err) { callback(err, null); return }
+
+    // otherwise, create the SBOM
+    const bom = new Bom(pkgInfo, componentType, includeSerialNumber, includeLicenseText, lockFileContents)
+
+    // pass the new bom to the callback
+    callback(null, bom)
+  });
+};
+
+
+createbom = (componentType, includeSerialNumber, includeLicenseText, path, options, callback) => readInstalled(path, options, (err, pkgInfo) => {
+  if (err) { callback(err, null); return }
+  const lockfileContents = readPackageLock(path);
+  const bom = new Bom(pkgInfo, componentType, includeSerialNumber, includeLicenseText, lockfileContents)
   callback(null, bom)
-})
+});
 
-exports.mergebom = function mergebom (doc, additionalDoc) {
+mergebom = function mergebom (doc, additionalDoc) {
   const additionalDocComponents = additionalDoc.getElementsByTagName('component')
   // appendChild actually removes the element from additionalDocComponents
   // which is why we use a while loop instead of a for loop
@@ -55,3 +87,10 @@ exports.mergebom = function mergebom (doc, additionalDoc) {
   }
   return true
 }
+
+module.exports = [
+  createbomFromFile,
+  createbomFromText,
+  createbom,
+  mergebom,
+]
